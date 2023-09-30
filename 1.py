55b6e80a1f3ee862ad8f3f15742bdea086c69d3f
MihailShernish <146514145+MihailShernish@users.noreply.github.com>
2023-09-30 17:31:39 +0900

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,21 +1,36 @@
+import discord
 import random
 
- 
+def gen_pass():
+    elements = "+-/*!&$#?=@<>"
+    password = ""
 
-rp = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    for i in range(10):
+        password += random.choice(elements)
+    return password
 
- 
+# Переменная intents - хранит привилегии бота
+intents = discord.Intents.default()
+# Включаем привелегию на чтение сообщений
+intents.message_content = True
+# Создаем бота в переменной client и передаем все привелегии
+client = discord.Client(intents=intents)
 
-pas = int(input('введи длинну пароля: '))
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
 
- 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('pass'):
+        await message.channel.send("genn_pass")
+    else:
+        await message.channel.send(message.content)
 
-finalpas = ''
+client.run("MTE1NTA2NjcyOTA0MzU5NTMzNQ.GQGUOT.Gj64GR2IMiWXtad2i7p_UpGytDpaN_cPj-6SyQ")
 
- 
-
-for i in range(pas):
-
-    finalpas += random.choice(rp)
-
-print(finalpas)
+#Новая функция гитхаб сделал
